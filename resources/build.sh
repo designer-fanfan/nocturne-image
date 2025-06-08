c5ed61b8018c64b7417cba73e50c1be16c5d9a4a
designer-fanfan <61039440+designer-fanfan@users.noreply.github.com>
2025-06-09 04:08:45 +0800

    Update build.sh

--- resources/build.sh
+++ resources/build.sh
@@ -41,7 +41,7 @@ ALPINE_BRANCH=$(echo "$ALPINE_BRANCH" | sed '/^[0-9]/s/^/v/')
 #chmod -R 755 "$WORK_PATH"
 # static config
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-WORK_PATH="/work"
+WORK_PATH="/workdir"
 IMAGE_PATH="${WORK_PATH}/img"
 export RES_PATH="${WORK_PATH}/resources"
 DEF_STAGE_PATH="${RES_PATH}/scripts/stages"
